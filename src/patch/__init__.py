a42155c7f86bdc734b2397fdc3bb06b39d7a6d25
diverHansun <3582444514@qq.com>
2025-09-06 18:49:10 +0800

    新增补丁

---
+++ src/patch/__init__.py
@@ -0,0 +1,9 @@
+"""
+Patch module for extending and fixing third-party libraries
+"""
+
+from .json_react_parser import JSONReActSingleInputOutputParser
+
+__all__ = [
+    "JSONReActSingleInputOutputParser"
+]
